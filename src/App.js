c57f68decdf0dfd3179933bc8a2360e564c69e4a
DanielONobre <daniel.cfn2011@gmail.com>
2025-08-28 00:15:48 -0300

    Avançando nas noções de props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import List from './components/list';
+import Frase from './components/Frase';
+import Item from './Item';
+import React from 'react';
+import Pessoa from './components/Pessoa';
 
 function App() {
   return (
     <div className="App">
       <h1>Meu Projeto React</h1>
       <p>Meu primeiro app</p>
+      <List />
     </div>
   );
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,5 +1,16 @@
+import Item from "./Item";
+
 function List(){
-    return <h1>Componente Lista</h1>
+    return(
+        <>
+            <h1>Minha Lista</h1>
+            <ul>
+                <Item marca="Ferrari" ano_lancamento={1985}/>
+                <Item marca="KIA" ano_lancamento={1999}/>
+                <Item marca="Renault" ano_lancamento={2005}/>
+            </ul>
+        </>
+    )
 }
 
 export default List;
