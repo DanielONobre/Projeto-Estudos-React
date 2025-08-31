b75de1e77ef61c81bf4c1407b7e4b85201a50259
DanielONobre <daniel.cfn2011@gmail.com>
2025-08-31 20:49:46 -0300

    Agora sim rs

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,46 @@
+import { useState } from "react"
+
 function Form() {
-    function cadastrarUsuario() {
-        e.preventDefault();
-        console.log("Cadastrou o usuário")
+    function cadastrarUsuario(e) {
+        e.preventDefault()
+        console.log("Cadastrou o usuário", { name, password })
     }
 
- return (
-    <div>
-        <h1>Meu Formulário</h1>
-        <form onSubmit={cadastrarUsuario}>
-            <div>
-                <input type="text" placeholder="Digite seu nome">Nome:</input>
-            </div>
-            <div>
-                <input type="submit" value="Cadastrar"></input>
-            </div>
-        </form>
-    </div>
- )
+    const [name, setName] = useState("")
+    const [password, setPassword] = useState("")
+
+    return (
+        <div>
+            <h1>Meu Cadastro</h1>
+            <form onSubmit={cadastrarUsuario}>
+                <div>
+                    <label htmlFor="name">Nome:</label>
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        placeholder="Digite seu nome"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Senha:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="Digite sua senha"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <input type="submit" value="Cadastrar" />
+                </div>
+            </form>
+        </div>
+    )
 }
 
- export default Form
+export default Form
